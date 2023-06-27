6c35b13e3f36a6da5d454c7750df1a5e61d47bdc
topjohncian <45138629+topjohncian@users.noreply.github.com>
2023-06-27 11:36:28 +0000

    Feat: watch build

--- packages/component-vue/components/TheWelcome.vue
+++ packages/component-vue/components/TheWelcome.vue
@@ -12,7 +12,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>Documentation Component</template>
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
