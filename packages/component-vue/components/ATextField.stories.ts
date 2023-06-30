4f9db6040cbb8f921d79d5facb37460c10ca9739
Hanmo5888 <letsgo@88.com>
2023-06-30 19:56:20 +0800

    perf: 优化 storybook 组件配置

--- packages/component-vue/components/ATextField.stories.ts
+++ packages/component-vue/components/ATextField.stories.ts
@@ -1,23 +1,27 @@
-import { ref } from 'vue'
-import ATextField from './ATextField.vue'
+import type { Meta, StoryObj } from "@storybook/vue3";
+import ATextField from "./ATextField.vue";
+import { ref } from "vue";
 
-export default {
+const meta: Meta<typeof ATextField> = {
     title: 'Components/ATextField',
-    component: ATextField,
+    component: ATextField
+};
+
+export default meta;
+type Story = StoryObj<typeof ATextField>;
+
+export const Default: Story = {
+    render: (args) => ({
+        components: { ATextField },
+        setup: () => {
+            const model = ref('');
+            const updateModel = (value: any) => model.value = value
+
+            return { args, model, updateModel }
+        },
+        template: '<ATextField v-bind="args" v-model="model" /><div>{{ model }}</div>'
+    }),
     args: {
-        label: '用户名',
-    }
-}
-
-const Template = (args: any) => ({
-    components: { ATextField },
-    setup() {
-        const model = ref('')
-        const updateModel = (value: any) => model.value = value
-
-        return { args, model, updateModel }
+        label: '用户名'
     },
-    template: '<ATextField v-bind="args" v-model="model" /><span>{{ model }}</span>'
-})
-
-export const Default = Template.bind({})
+};
